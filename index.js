41eddf4d103e50e6d2ecb73c009a64fc493bc8d3
kerong2002 <krameri120@gmail.com>
2023-03-30 16:21:01 +0800

    test

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const { Toolkit } = require('actions-toolkit');
 Toolkit.run(async (tools) => {
     tools.log.success("Edit README.md Start...");
 
-    tools.log.success(`Updated README with ${cppFiles.length} C++ files`);
+    tools.log.exit("Exit Edit README.md Action");
 });
